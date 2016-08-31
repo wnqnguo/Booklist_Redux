f1878af23da841cdeff4032b66d7977b07699b60
Wenqian Guo <wnqnguo@gmail.com>
2016-08-30 20:53:18 -0700

    changed book-list to container instead of dumb component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import {} from 'react-redux'
+import BookList from '../containers/book-list'
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <BookList />
     );
   }
 }
