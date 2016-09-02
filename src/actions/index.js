2150c50628af2a320babcd9d59b0706068437d38
Wenqian Guo <Wenqian@Wenqians-MacBook-Pro.local>
2016-09-02 15:51:49 -0700

    action creator returns action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book){
-	console.log( 'A book has been selected ', book.title);
+	//selectBook is an ActionCreate, it needs to return an action
+	//an object with a type property
+	return {
+		type: 'BOOK_SELECTED',
+		payload: book
+	};
 }
