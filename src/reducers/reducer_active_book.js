b6acfb35bb3e91b2e82f6f59e1a7de797eeb86b8
Wenqian Guo <wnqnguo@gmail.com>
2016-09-02 16:15:44 -0700

    set state to null if it's undefined in active book reducer

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,5 +1,10 @@
 //state arument is not application state. 
 //only the state the reducer is responsible for
-export default function(state, action){
+// es6 syntax, if state is undefined set it to null
+export default function(state = null, action){
+	switch(action.type){
+		case 'BOOK_SELECTED':
+			return action.payload;
+	}
 	return state
 }
