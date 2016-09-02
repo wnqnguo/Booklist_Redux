f823c0330812c94456b472ac9f61d4294a78ddba
Wenqian Guo <wnqnguo@gmail.com>
2016-09-02 16:36:40 -0700

    update book detail to render book title

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,6 +1,21 @@
 import React, {Component} from 'react';
-export default class BookDetail extends Component{
+import {connect} from 'react-redux';
+class BookDetail extends Component{
 	render(){
-		return (<div>BookDetail</div>);
+		if(!this.props.book){
+			return <div>Select a book to get started.</div>
+		}
+		return (
+			<div>
+			<h3>Details for:</h3>
+			<div>{this.props.book.title}</div>
+			</div>);
 	}
 }
+function mapStateToProps(state){
+	return{
+		book: state.activeBook
+	};
+}
+
+export default connect(mapStateToProps)(BookDetail);
